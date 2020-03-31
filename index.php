779b05e723a51cf2088a60793faf5b358c3a5e45
Benkaera <50444630+Benkaera@users.noreply.github.com>
2020-03-31 15:59:10 +0300

    okey

--- index.php
+++ index.php
@@ -2,18 +2,14 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<!-- /Сжатие сайта для мобильных устройств -->
 		<meta name="viewport" content="width=device-width , initial-scale=1.0">
-		//Сжатие сайта для мобильных устройств
-		<title></title>
+		<meta http-http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<title>Торговый центр WestPlus</title>
 	</head>
 	<body>
 
-	</body>
+</body>
 </html>
-
-
-drdd
-ty
-rrrrrr
-333333333333
-gfgggg
