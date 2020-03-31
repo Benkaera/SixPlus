0f9620f212a0cc818913be974766a2f93e4e56e1
Benkaera <50444630+Benkaera@users.noreply.github.com>
2020-03-31 21:26:43 +0300

    night1

--- index.php
+++ index.php
@@ -10,6 +10,45 @@
 		<title>Торговый центр WestPlus</title>
 	</head>
 	<body>
+		<!-- Шапка -->
+		<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">WestPlus</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="#">Главная</a>
+    <a class="p-2 text-dark" href="#">Контакты</a>
+    <a class="p-2 text-dark" href="#">Товары</a>
+      </nav>
+  <a class="btn btn-outline-primary" href="#">Войти</a>
+</div>
+<div class="container mt-5">
 
+<h3 class="mb-5">Наша Статья</h3>
+
+
+<div class="d-flex flex-wrap">
+<?php
+for($i=0;$i<5;$i++):
+?>
+
+<!-- блок1 -->
+<div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Блок</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$100 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Адьбом KAROM</li>
+          <li>2020</li>
+          <li>Поддержка исполнителей</li>
+          <li>Запись бесплатно</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Тык</button>
+      </div>
+    </div>
+	<?php endfor; ?>
+</div>
+
+</div>
 </body>
 </html>
