ab4dd2cd1efb1e0175117a518fdaa627f08e461f
Benkaera <50444630+Benkaera@users.noreply.github.com>
2020-03-31 12:19:49 +0300

    testaa

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width , initial-scale=1.0">
+		//Сжатие сайта для мобильных устройств
+		<title></title>
+	</head>
+	<body>
+
+	</body>
+</html>
+
+
+drdd
+ty
+rrrrrr
+333333333333
